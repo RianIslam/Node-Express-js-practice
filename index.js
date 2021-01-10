f1cc766ee78957794f0838cefc2146689d16894d
RianIslam <rianislamdr@gmail.com>
2021-01-11 03:39:17 +0600

    res.body

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
-const express =require('express');
- const app = express();
+// const express = require("express");
+// const app = express();
 
- app.get('/',(req,res) =>{
+// app.use(express.json());
+// app.get("/", (req, res) => {
+//   res.send("this is home page");
+// });
 
- });
+// app.post("/", (req, res) => {
+//   console.log(req.body);
+//   res.send("this is home page with post request");
+// });
+// app.listen(3000, () => {
+//   console.log("listening on port 3000");
+// });
 
- app.listen(3000,()=>{
-     console.log('listening on port 3000');
- })
+
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send('this is home page with get request');
+})
+
+app.post('/',(req,res) =>{
+    console.log(res.body);
+    res.send('This is home page with post request');
+})
+app.listen(3000,()=>{
+    console.log('listening on port 3000');
+})
