700d2c8c7b7db7144eeaf33e4ee2c52329fe789e
RianIslam <rianislamdr@gmail.com>
2021-01-11 04:05:39 +0600

    static

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static(`${__dirname} + /public/`));
+
+app.get("/", (req, res) => {
+  res.send("this is home page");
+});
+
+app.post("/", (req, res) => {
+  res.send("this is home page with post request");
+});
+
+app.listen(3000, () => {
+  console.log("lisning on port 3000");
+});
+
+
+
 // const express = require("express");
 // const app = express();
 
-// app.use(express.json());
+// app.use(express.text());
 // app.get("/", (req, res) => {
 //   res.send("this is home page");
 // });
@@ -14,22 +34,20 @@
 //   console.log("listening on port 3000");
 // });
 
+// const express = require('express');
 
+// const app = express();
 
-const express = require('express');
+// app.use(express.json());
 
-const app = express();
+// app.get("/", (req, res) => {
+//   res.send("this is home page with get request");
+// });
 
-app.use(express.json());
-
-app.get('/',(req,res)=>{
-    res.send('this is home page with get request');
-})
-
-app.post('/',(req,res) =>{
-    console.log(res.body);
-    res.send('This is home page with post request');
-})
-app.listen(3000,()=>{
-    console.log('listening on port 3000');
-})
+// app.post("/", (req, res) => {
+//   console.log(res.body.name);
+//   res.send("This is home page with post request");
+// });
+// app.listen(3000, () => {
+//   console.log("listening on port 3000");
+// });
