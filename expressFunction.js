ee8011c78e10bf07ae9e1afe25d3840cedcd2eea
RianIslam <rianislamdr@gmail.com>
2021-02-06 20:36:53 +0600

    convert json to object

--- expressFunction.js
+++ expressFunction.js
@@ -1,14 +1,19 @@
 const express = require('express');
+const { type } = require('os');
 const app  = express();
+
+app.use(express.json())
+
 
 app.get('/',(req,res) => {
     res.send('this is homepage');
 });
 
-app.post('/',(req,res){
+app.post('/',(req,res) =>{
+    console.log(req.body.name)
     res.send('this is body with home page');
 });
 
-app.listen(3000,() =>{
-    console.log("listening on port 3000")
+app.listen(3001,() =>{
+    console.log("listening on port 3001")
 })
