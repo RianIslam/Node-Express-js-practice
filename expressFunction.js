e7279583e691732aae3d410020ebefd7bcd151b7
RianIslam <rianislamdr@gmail.com>
2021-02-06 20:49:49 +0600

    text/plain

--- expressFunction.js
+++ expressFunction.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { type } = require('os');
 const app  = express();
 
-app.use(express.json())
+app.use(express.text())
 
 
 app.get('/',(req,res) => {
@@ -10,7 +10,7 @@ app.get('/',(req,res) => {
 });
 
 app.post('/',(req,res) =>{
-    console.log(req.body.name)
+    console.log(req.body)
     res.send('this is body with home page');
 });
 
